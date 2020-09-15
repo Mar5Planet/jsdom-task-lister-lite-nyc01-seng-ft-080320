f39725a829f2b2f05e1e4e76b84260fccce4d83c
Mar5Planet <amustaf@email.arizona.edu>
2020-09-15 14:07:55 -0400

    Deliverables Met

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  
+  const taskForm = document.querySelector('#create-task-form');
+  const ulElement = document.querySelector('#tasks');
+ 
+  taskForm.addEventListener('submit', function(e){ 
+    e.preventDefault();
+
+    const inputValue = document.querySelector('#new-task-description').value.trim();
+
+    const taskItem = document.createElement('li')
+    taskItem.innerText = inputValue
+
+    ulElement.append(taskItem)
+
+    taskForm.reset()
+
+
+   });
+
+ 
+
+  
 });
